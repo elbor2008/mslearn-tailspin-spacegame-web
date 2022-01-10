915a0c5ed2ad851ed14ee5139f4f2ce86b4f104c
Nick Nie <nicnie@deloitte.com.au>
2022-01-10 20:40:34 +1100

    correct test case

--- Tailspin.SpaceGame.Web.Tests/DocumentDBRepository_GetItemsAsyncShould.cs
+++ Tailspin.SpaceGame.Web.Tests/DocumentDBRepository_GetItemsAsyncShould.cs
@@ -54,7 +54,7 @@ namespace Tests
 
         [TestCase(0, ExpectedResult = 0)]
         [TestCase(1, ExpectedResult = 1)]
-        [TestCase(10, ExpectedResult = 11)]
+        [TestCase(10, ExpectedResult = 10)]
         public int ReturnRequestedCount(int count)
         {
             const int PAGE = 0; // take the first page of results
